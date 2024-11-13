95092d232f48c278a3f79b03b0656f7280c3d9a5
suhaniishah <129926451+suhaniishah@users.noreply.github.com>
2024-11-13 21:31:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">= 0.12"
 }
+
 provider "local" {
   alias = "local"
 }
 
+resource "local_file" "example" {
+  content  = "Hello, Terraform!"
+  filename = "${path.module}/hello.txt"
+}
